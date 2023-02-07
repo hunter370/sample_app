3cfabdd3cb2eac47c66ed03707a707d9b1c2e0ea
EC2 Default User <ec2-user@ip-172-31-16-150.ap-northeast-1.compute.internal>
2023-02-07 04:49:03 +0000

    [Finish] 3-4

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/edit'
+  get '/top' => 'homes#top'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
+  get 'lists/:id' => 'lists#show', as: 'list'
+end
